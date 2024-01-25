07cb16e654fa314c80edd4b573993bc9a2d93f28
Brian <brianandez6@gmail.com>
2024-01-25 02:03:05 -0600

    dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { DeveloperViewComponent } from './developer-view/developer-view.component';
 import { DeviceViewComponent } from './device-view/device-view.component';
 import { HistoryViewComponent } from './history-view/history-view.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
+    { path: '', component: DashboardComponent },
     { path: 'history', component: HistoryViewComponent },
     { path: 'state', component: DeviceViewComponent },
     { path: 'developer', component: DeveloperViewComponent }
